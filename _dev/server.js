d20cfd9e2b5760eab5360e0938523e2a4d640fd6
Markus Tran <thebluepillow@gmail.com>
2017-10-03 20:33:16 -0700

    dev server

---
+++ _dev/server.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = +process.argv[2] || 3000
+
+app.use(express.static(path.join(__dirname, '..')))
+
+app.listen(port, function () {
+  console.log(`App started on port ${port}`)
+})
