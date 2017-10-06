a6a1b37fc739ca6ca6a71f91e47eef481e7672b8
Markus Tran <thebluepillow@gmail.com>
2017-10-05 18:52:59 -0700

    fix web structure

--- _dev/server.js
+++ _dev/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = +process.argv[2] || 3000
 
-app.use(express.static(path.join(__dirname, '..')))
+app.use('/~20198403', express.static(path.join(__dirname, '..')))
 
 app.listen(port, () => {
   console.log(`App started on port ${port}`)
