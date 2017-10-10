e3fbbc535515f9f3bd3ca1a93569c3f1742c2352
Markus Tran <thebluepillow@gmail.com>
2017-10-10 15:54:39 -0700

    init deploy

---
+++ _dev/deploy/config.sample.php
@@ -0,0 +1,8 @@
+<?php
+define("TOKEN", "secret-token");
+define("REMOTE_REPOSITORY", "git@github.com:pillowfication/cis89c.git");
+define("DIR", "/../../");
+define("BRANCH", "refs/heads/master");
+define("LOGFILE", "deploy.log");
+define("GIT", "/usr/bin/git");
+// define("AFTER_PULL", "");
