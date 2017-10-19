4b3a5f3f95b7ee714e27bd7c117123bd60127f1d
Markus Tran <thebluepillow@gmail.com>
2017-10-19 16:09:32 -0700

    add foundation

--- _dev/website/index.jsx
+++ _dev/website/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import './main.scss'
+import './css/main.scss'
 
 import HelloWorld from './components/HelloWorld.jsx'
 
